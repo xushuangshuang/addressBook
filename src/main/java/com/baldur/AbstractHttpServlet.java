4a017b084f6146eaa8406c629610f6e7f99cf12a
Dev--xu <xushuangshuang1@gmail.com>
2014-07-20 16:06:09 +0800

    将可能造成异常的方法提出来进行处理

--- src/main/java/com/baldur/AbstractHttpServlet.java
+++ src/main/java/com/baldur/AbstractHttpServlet.java
@@ -10,24 +10,32 @@ public class AbstractHttpServlet extends HttpServlet
         Member member = new Member();
 
         String username = req.getParameter("username");
-        Long mobile = Long.valueOf(req.getParameter("mobile"));
-        int vpmn = Integer.valueOf(req.getParameter("vpmn"));
+        String parmMobile = req.getParameter("mobile");
+        String parmVpmn = req.getParameter("vpmn");
         String email = req.getParameter("email");
         String home_address = req.getParameter("home_address");
         String office_address = req.getParameter("office_address");
         String memo = req.getParameter("memo");
         String groups = req.getParameter("groups");
         String job = req.getParameter("job");
-        int job_level = Integer.valueOf(req.getParameter("job_level"));
+        String parmJob_level = req.getParameter("job_level");
         
         member.setUsername(username);
-        member.setMobile(mobile);
         member.setEmail(email);
         member.setGroups(groups);
         member.setHomeAddress(home_address);
         member.setOfficeAddress(office_address);
         member.setJob(job);
-        member.setVpmn(vpmn);
-        member.setJobLevel(job_level);
+        member.setMemo(memo);
+
+        if(Util.assertNull(parmMobile, parmVpmn, parmJob_level))
+        {
+            Long mobile = Long.valueOf(parmMobile);
+            member.setMobile(mobile);
+            int vpmn = Integer.valueOf(parmVpmn);
+            member.setVpmn(vpmn);
+            int job_level = Integer.valueOf(parmJob_level);
+            member.setJobLevel(job_level);
+        }
     }
 }
