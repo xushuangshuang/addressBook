b11d8cf418ff1e7a5c169cad86bc78d7d798bdd4
Dev--xu <xushuangshuang1@gmail.com>
2014-07-20 16:08:48 +0800

    将对获取到的数据进行判断其是否为空

--- src/main/java/com/baldur/Util.java
+++ src/main/java/com/baldur/Util.java
@@ -2,10 +2,11 @@ package com.baldur;
 
 public class Util
 {
-    public static boolean assertNull(String param)
+    public static boolean assertNull(String... param)
     {
         if(null == param)
         {
+            System.out.println("信息不完整");
             return false;
         }
         else
