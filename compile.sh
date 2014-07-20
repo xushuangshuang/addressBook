a6d5597e543f71534d5fc50a5422b8d0c674cbd1
Dev--xu <xushuangshuang1@gmail.com>
2014-07-20 17:00:22 +0800

    增加了编码属性的添加

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ set e
 
 mkdir -p target/classes
 
-javac -d target/classes/  -classpath lib/servlet-api.jar src/main/java/com/baldur/* 
+javac -encoding "UTF-8" -d target/classes/  -classpath lib/servlet-api.jar src/main/java/com/baldur/* 
 
 echo "COMPILE SUCCESS"
