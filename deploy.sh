e42d2cc709b18040224dc6c4df833ae10d8928a4
Dev--xu <xushuangshuang1@gmail.com>
2014-07-20 11:20:19 +0800

    addressBook目录已经存在，不需要再建立

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mkdir $CATALINA_HOME/webapps/addressBook
-
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/addressBook
 
 cp -r target/classes/* $CATALINA_HOME/webapps/addressBook/WEB-INF/classes
