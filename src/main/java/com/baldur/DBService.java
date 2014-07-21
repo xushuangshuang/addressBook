aaf26a1fdeda20c7d9b3cb3f3c71c6c6b22672aa
Dev--xu <xushuangshuang1@gmail.com>
2014-07-21 22:41:54 +0800

    增加了数据库中的service方法

---
+++ src/main/java/com/baldur/DBService.java
@@ -0,0 +1,74 @@
+package com.baldur;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class DBService
+{   
+    private static final String jdbcURL = "jdbc:mysql://localhost/test?" 
+        + "user=root&password=";
+    private static final String jdbcDriver = "com.mysql.jdbc.Driver";
+    private Connection conn = null;    
+    private PreparedStatement  pstmt = null;
+    private int paremtIndex;
+
+    public DBService prepare(String sql) throws SQLException
+    {
+        pstmt = conn.prepareStatement(sql);
+        paremtIndex = 1;
+        return this;    
+    }
+
+    public DBService setString(String param) throws SQLException
+    {
+        pstmt.setString(paremtIndex, param);
+        paremtIndex++;
+        return this;
+    }
+    public DBService setInt(int param) throws SQLException
+    {
+        pstmt.setInt(paremtIndex, param);
+        paremtIndex++;
+        return this;
+    }
+    public DBService setLong(Long param) throws SQLException
+    {
+        pstmt.setLong(paremtIndex, param);
+        paremtIndex++;
+        return this;
+    }
+    public void execute() throws SQLException
+    {
+        pstmt.execute();
+    }
+    public static Connection createConnection() throws SQLException
+    {
+        try
+        {
+            Class.forName(jdbcDriver);
+        }
+        catch(Exception e)
+        {
+
+        }
+        return DriverManager.getConnection(jdbcURL);
+    }
+
+    public static DBService newInstance()
+    {
+        DBService db = new DBService();
+        try
+        {
+            db.conn = DBService.createConnection();
+        }
+        catch(Exception e)
+        {
+            
+        }
+        return db;
+
+    }
+
+}
